198dff01dedac4c10b06d71bd3817c670de2152a
kopianan <ananalfredcarlos@gmail.com>
2021-05-22 10:52:53 +0700

    commit

---
+++ lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import 'package:flutter2/presentation/app_widget.dart';
+
+void main() {
+  runApp(AppWidget());
+}
---
+++ lib/presentation/home/home_page.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  static final String TAG = '/';
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
